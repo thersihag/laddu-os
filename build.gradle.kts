b40860d523508e5d5ee657af196c4623b4fa072c
thersihag <rsihagds@gmail.com>
2026-06-19 21:47:04 +0530

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Path: build.gradle.kts (Root Level Verification)
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
