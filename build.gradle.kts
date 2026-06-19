32b90bc87dff1246448d5d053b204880af032737
thersihag <rsihagds@gmail.com>
2026-06-19 14:12:16 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Path: build.gradle.kts
+// Path: build.gradle.kts (Root Level Verification)
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
